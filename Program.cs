57e7d0cfb5dcab6c4f0cf70791fe42823184fb0b
Bruno Donadei <domanbruno@gmail.com>
2020-11-17 16:35:01 -0200

    finalizado a tabuada

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deseja saber a tabuada de qual número?");
+            int resposta = int.Parse(Console.ReadLine());
+
+            int resultado;
+
+            for (int contador = 0; contador <= 10; contador ++)
+            {
+                resultado = resposta * contador;
+                Console.WriteLine($"{resposta} * {contador} = {resultado}");
+            }
+        }
+    }
+}
